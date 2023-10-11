1f5a4bc748171a68975b933fc0316b0bfbc46829
MehemmedBulut <mehemmedbulut@gmail.com>
2023-10-11 21:21:09 +0400

    Add project files.

---
+++ Infrastructure/ECommerceAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,31 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Persistance.Contexts;
+using ECommerceAPI.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this
+            IServiceCollection services)
+        {
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.ConnectionString),
+                ServiceLifetime.Singleton);
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+        }
+    }
+}
---
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+
+        readonly private IOrderWriteRepository _orderWriteRepository;
+        readonly private ICustomerWriteRepository _customerWriteRepository;
+        readonly private IOrderReadRepository _orderReadRepository;
+        public ProductsController(IProductWriteRepository productWriteRepository,IProductReadRepository productReadRepository, IOrderWriteRepository orderWriteRepository, ICustomerWriteRepository customerWriteRepository, IOrderReadRepository orderReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerWriteRepository = customerWriteRepository;
+            _orderReadRepository = orderReadRepository;
+        }
+        [HttpGet]
+        public async Task Get()
+        {
+            Order order =await _orderReadRepository.GetByIdAsync("1e804e13-345a-47c4-2777-08dbc9af3b82");
+            order.Address = "Istanbul";
+            await _orderWriteRepository.SaveAsync();
+        }
+
+    }
+}
